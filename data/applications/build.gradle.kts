a40676c82ed7d639d8b6466308ce40aa5281a44b
Alexander <like81@yandex.ru>
2021-12-14 16:17:20 +0300

    added data

---
+++ data/applications/build.gradle.kts
@@ -0,0 +1,11 @@
+kotlin {
+    sourceSets {
+        val main by getting
+        val test by getting
+    }
+}
+
+dependencies {
+    api(project(":data:base"))
+//    api(project(":domain:applications"))
+}
