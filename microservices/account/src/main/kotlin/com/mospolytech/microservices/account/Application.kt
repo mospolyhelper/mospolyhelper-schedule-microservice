864a1f80524cb8b8ef5f637c2303437e73da6b4b
Alexander <like81@yandex.ru>
2021-12-16 15:55:30 +0300

    payments type converter

--- microservices/account/src/main/kotlin/com/mospolytech/microservices/account/Application.kt
+++ microservices/account/src/main/kotlin/com/mospolytech/microservices/account/Application.kt
@@ -3,9 +3,11 @@ package com.mospolytech.microservices.account
 
 import com.mospolytech.features.applications.applicationsRoutesV1
 import com.mospolytech.features.base.koin.get
+import com.mospolytech.features.payments.paymentsDataConversion
 import com.mospolytech.features.payments.paymentsRoutesV1
 import com.mospolytech.microservices.account.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
@@ -19,9 +21,16 @@ fun Application.module() {
     configureHTTP()
     configureDi()
     setRoutes()
+    setDataConversions()
 }
 
 fun Application.setRoutes() {
     applicationsRoutesV1(get())
     paymentsRoutesV1(get())
+}
+
+fun Application.setDataConversions() {
+    install(DataConversion) {
+        paymentsDataConversion()
+    }
 }
