34943bc43ff252d1501d96475ce1ccab45823f57
Alexander <like81@yandex.ru>
2021-12-18 15:26:36 +0300

    marks

--- microservices/account/src/main/kotlin/com/mospolytech/microservices/account/AppModules.kt
+++ microservices/account/src/main/kotlin/com/mospolytech/microservices/account/AppModules.kt
@@ -3,12 +3,15 @@ package com.mospolytech.microservices.account
 import com.mospolytech.data.applications.applicationsDataModule
 import com.mospolytech.data.base.baseDataModule
 import com.mospolytech.data.payments.paymentsDataModule
+import com.mospolytech.data.performance.performanceDataModule
 import com.mospolytech.data.personal.personalDataModule
 import com.mospolytech.domain.applications.applicationsDomainModule
 import com.mospolytech.domain.payments.paymentsDomainModule
+import com.mospolytech.domain.perfomance.performanceDomainModule
 import com.mospolytech.domain.personal.personalDomainModule
 import com.mospolytech.features.applications.applicationsFeatureModule
 import com.mospolytech.features.payments.paymentsFeatureModule
+import com.mospolytech.features.performance.performanceFeaturesModule
 import com.mospolytech.features.personal.personalFeaturesModule
 
 val appModules = listOf(
@@ -21,5 +24,8 @@ val appModules = listOf(
     paymentsFeatureModule,
     personalDataModule,
     personalDomainModule,
-    personalFeaturesModule
+    personalFeaturesModule,
+    performanceDataModule,
+    performanceDomainModule,
+    performanceFeaturesModule
 )
