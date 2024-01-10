1924052ad93142e6df77ceec9a3808e4c1171a9a
Andrey Bogomolov <tipapro@mail.ru>
2024-01-10 17:39:06 +0300

    Cors

--- features/base/src/main/kotlin/com/mospolytech/features/base/plugins/HTTP.kt
+++ features/base/src/main/kotlin/com/mospolytech/features/base/plugins/HTTP.kt
@@ -1,16 +1,14 @@
 package com.mospolytech.features.base.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
-//    install(CORS) {
-//        method(HttpMethod.Options)
-//        method(HttpMethod.Put)
-//        method(HttpMethod.Delete)
-//        method(HttpMethod.Patch)
-//        header(HttpHeaders.Authorization)
-//        header("MyCustomHeader")
-//        allowCredentials = true
-//        //anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-//    }
+    install(CORS) {
+        // TODO remove
+        allowHost("localhost:8080")
+        allowHost("edugma.github.io")
+        allowHeader(HttpHeaders.ContentType)
+    }
 }
