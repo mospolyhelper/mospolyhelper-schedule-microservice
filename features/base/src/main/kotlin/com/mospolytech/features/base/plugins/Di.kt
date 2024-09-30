bdc21e287cc1a6be7c907eae05713f3348546f49
tipapro <tipapro@mail.ru>
2024-09-30 04:58:02 +0300

    Fix

--- features/base/src/main/kotlin/com/mospolytech/features/base/plugins/Di.kt
+++ features/base/src/main/kotlin/com/mospolytech/features/base/plugins/Di.kt
@@ -2,13 +2,11 @@ package com.mospolytech.features.base.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.config.*
-import org.koin.core.context.GlobalContext
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
 fun Application.configureDi(modules: List<org.koin.core.module.Module>) {
     install(Koin) {
         modules(modules.plus(module { single<ApplicationConfig> { environment.config } }))
-        GlobalContext.startKoin(this)
     }
 }
